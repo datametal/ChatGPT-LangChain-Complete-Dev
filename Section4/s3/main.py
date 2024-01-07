86e632b46f340cc721c89fdc47e9fa00205070ce
Barko <brk0@waroffice.us>
2024-01-07 05:32:08 -0500

    feat: section five

--- Section4/s3/main.py
+++ Section4/s3/main.py
@@ -1,8 +1,14 @@
-from langchain.document_loaders import TextLoader
+import os
+
 from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader
 
 load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+if api_key is None:
+    raise Exception("OPENAI_API_KEY not found in .env file")
 
+# chat = ChatOpenAI(api_key=api_key, verbose=True)
 loader = TextLoader("facts.txt")
 docs = loader.load()
 
--- Section4/s5/main.py
+++ Section4/s5/main.py
@@ -1,20 +1,19 @@
+# import os
+from pprint import pprint as pp
+
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+# api_key = os.getenv("OPENAI_API_KEY")
+# if api_key is None:
+#     raise Exception("OPENAI_API_KEY not found in .env file")
 
-text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=200,
-    chunk_overlap=0
-)
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
 
 loader = TextLoader("facts.txt")
-docs = loader.load_and_split(
-    text_splitter=text_splitter
-)
+docs = loader.load_and_split(text_splitter=text_splitter)
 
 for doc in docs:
-    print(doc.page_content)
-    print("\n")
+    pp(doc.page_content)
+    pp("\n")
